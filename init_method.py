5f5353e53ba0d3c614fa4727936ff37d3db9c87f
Adilkhan <adilkhankumar8@gmail.com>
2026-04-06 18:31:37 +0500

    first commit

---
+++ init_method.py
@@ -0,0 +1,36 @@
+class People:
+  def __init__(self,country,address):
+      self.country = country
+      self.address = address
+
+d1=People("Kazakhstan","seifullin")
+print(d1.country)
+print(d1.address)
+
+class Person:
+   def __init__(self,name,age):
+      self.name=name
+      self.age = age
+
+l1=Person("Adilkhan",15)
+print(l1.name)
+print(l1.age)
+
+class People:
+  def __init__(self,city,address):
+      self.city = city
+      self.address = address
+
+d1=People("Almaty","seifullin")
+print(d1.city)
+print(d1.address)
+
+
+class People:
+  def __init__(self,grade,hobby):
+      self.grade = grade
+      self.hobby = hobby
+
+d1=People(4,"Play volleyball")
+print(d1.grade)
+print(d1.hobby)
